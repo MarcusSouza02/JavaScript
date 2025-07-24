a94f54560be63d20644f78be1e3f00c8368f482d
Marcus Vinicius <111466820+MarcusSouza02@users.noreply.github.com>
2025-07-24 12:45:07 -0300

    Update index.js

--- curso de JS/index.js
+++ curso de JS/index.js
@@ -283,7 +283,6 @@ if(interruptor == "on"){
 }else {
     alert('A lampada está desligada')
 }
- */
 
 var hora = new Date().getHours; // pega a hora atual do sistema
 
@@ -295,5 +294,249 @@ if (hora < 12){
     alert("Boa noite");
 }
 
-//3:29:44
+function verificar(){
+    let nome = document.getElementById("nome").value;
 
+    if (nome == "" || nome == null){
+        let p = document.getElementById("teste");
+        p.innerHTML = "O campo não pode ser vazio";
+        p.style.color = "red";
+    }else{
+        let p = document.getElementById("teste");
+        p.innerHTML = "Tudo certo";
+        p.style.color = "green";
+    }
+}
+ */
+
+/**
+ Aula 14 - Switch 
+ O Switch é literal, compara também os tipos e não só os valores
+
+  function verificaCor(){
+    let cor = document.getElementById("cor").value;
+    cor = cor.toLowerCase(); //Muda tudo para minusculo para não dar erro 
+
+    switch(cor){ //verifica a cor inserida pelo usuário e coloca a mesma cor no background
+        case "azul":
+           document.body.style.backgroundColor = "blue";
+           break;
+        case "vermelho":
+            document.body.style.backgroundColor = "red";
+            break;
+        case "amarelo":
+            document.body.style.backgroundColor = "yellow";
+            break;
+        default:
+            document.getElementById("teste").innerHTML = "Nenhuma cor disponível para " + cor;
+    }
+ }
+ */
+
+/**
+ Aula 15 - FOR
+
+ for (let i = 0; i <10001; i++){
+    document.getElementById("teste").innerHTML += i + ", ";
+ }
+
+
+ var ano = new Date().getFullYear();
+
+ for(let i = ano; i >= 1900; i--){
+    document.getElementById("ano").innerHTML += "<option value = " + i + " > " + i + "</option>";
+ }
+
+const carros = ["Gol", "Fusca", "Ka", "Onix", "Chevette"];
+
+var tamanho = carros.length;
+
+for(let i = 0; i < tamanho; i++){
+    document.getElementById("teste").innerHTML += carros[i] + " ";
+}
+
+ */
+
+/**
+ Aula 16 - SetTimout e SetInterval
+ eventos de tempo/cronometragem
+ servem para executar o programa em tempos especificos 
+
+ SetTimout (function, tempo em milisegundos)
+    -> Executa depois de esperar alguns milisegundos, executa uma vez só
+
+SetInterval (Function, milisegundos)
+    -> É a mesma coisa que SetTimout mas continua repetindo a função
+
+ function ativarConatagem(){
+    //document.getElementById("tempo").innerHTML = "Começou a contar!";
+    //Ativa a função uma vez quando der o tempo especificado
+    // tempo = setTimeout(function () {
+    //     document.getElementById('tempo').innerHTML = "Executou o SetTimeout";
+    // }, 5000);
+
+    tempo = setInterval(function ( ) { // a cada intervalo de tempo ela soma um, igual um cronometro
+    var cronometro = document.getElementById('tempo').innerHTML;
+    var soma = parseInt(cronometro) + 1;
+    document.getElementById('tempo').innerHTML = soma;
+ }, 1000)
+}
+
+
+function pararContagem(){
+    // clearTimeout(tempo); // cancela o setTimeout
+    // document.getElementById('tempo').innerHTML = "Parou a contagem";
+    clearInterval(tempo); // cancela a contagem do setInterval
+}
+}
+ */
+
+/**
+ Aula 17 - Classes
+
+  class Carro{
+    constructor(valor1, valor2, valor3){ //Construtor da classe
+        this.marca =  valor1;
+        this.modelo = valor2;
+        this.ano = valor3;
+    }
+
+    buzina(){ //Metodo da classe
+        return this.modelo + " buzinou: Bibibibibibi";
+    }
+ }
+
+ const uno =  new Carro("Fiat", "Uno", "2002");
+ const gol = new Carro("Volkswagen", "Gol", "2006");
+
+ console.log(uno);
+ console.log(uno.ano);
+ uno.ano = 2005; //Alterando um dado
+ console.log(uno.ano);
+ console.log(uno.buzina());
+ console.log(gol.buzina());
+ */
+
+ /**
+  Aula 18 - Manipulação de Datas
+
+// comando para pegar a data do dia atual
+let data = new Date();
+//console.log(data);
+
+//comando para pegar o ano com 4 digitos 
+//let ano = new data.getFullYear();
+//console.log(ano);
+
+//Pega o mes atual de 0 (Janeiro) a 11 (Dezembro)
+//let mes = data.getMonth();
+//console.log(mes);
+
+    
+//const mesesAno = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+//let mesEscrito = mesesAno[data.getMonth()];
+//console.log(mesEscrito);
+
+//Pega o dia do mes - 1 ate 31
+// let diaMes = data.getDate();
+// console.log(diaMes);
+
+//pegar horas de 0 até 23
+// let hora = data.getHours();
+// console.log(hora);
+
+//pegar os minutos de 0 ate 59
+// let minutos = data.getMinutes();
+// console.log(minutos)
+
+//pegar os segundos de 0 ate 59 
+// let segundos = data.getSeconds();
+// console.log(segundos);
+
+//pegar os milisegundos de 0 a 999
+// let milisegundos = data.getMilliseconds();
+// console.log(milisegundos);
+
+// pega a data completa no padrão BR
+let dataBR = data.toLocaleString('pt-BR', {dateStyle: 'short'}); // dateStyle: 'short' = pega so a data timeStyle: 'short' = pega a hora
+console.log(dataBR);
+
+//Comparar datas
+var hoje = new Date();
+var vencimento = new Date(2025, 6, 29); //new Date(ano, mes, dia)
+
+if(hoje > vencimento){
+    console.log('Sua conta está vencida!');
+}else { 
+    console.log("Ainda não venceu, tranquilo!")
+}
+
+//Difernça em dias entre duas datas 
+var dataInicial = new Date();
+var dataFinal = new Date(2025, 10, 14);
+
+var diferencaTempo =  dataFinal.getTime() - dataInicial.getTime();//tem que transformar para milisegundos
+
+var diferencaDias = Math.ceil(diferencaTempo/ (24 * 60 * 60 * 1000)); // calculo para transformar milisegundos em dias
+console.log(diferencaDias + " dias");
+
+  */
+
+
+
+/*
+ Aula 19 - JSON
+ converte objeto em texto e o contrário também
+
+ const carro = {
+    marca: "ford",
+    modelo: "Ka",
+    ano: 2015,
+    motor: ['1.6', '1.4', '1.0']
+}
+
+let texto = JSON.stringify(carro); //transformou o objeto carro em texto
+
+document.getElementById('area').innerHTML = texto; //colocou o texto no html
+
+let obj = JSON.parse(texto); //converteu texto em objeto
+
+console.log(obj.modelo); //pegamos um valor deste objeto
+
+console.log(obj.motor[2]); //pegamos um valor deste objeto
+
+ const ajax = new XMLHttpRequest(); //Request para acessar um site 
+ ajax.open('GET','https://viacep.com.br/ws/37536001/json/') //abriu esse link
+ ajax.send();
+
+ajax.onload = function(){ // Pegou o que tinha no link e colocou no html
+    document.getElementById('area').innerHTML = this.responseText;
+    let obj = JSON.parse(this.responseText);
+    alert(obj.ddd);
+ }
+
+function buscarCEP(){
+    let input = document.getElementById('cep').value;
+
+    const ajax = new XMLHttpRequest(); //Request para acessar um site 
+    ajax.open('GET','https://viacep.com.br/ws/' + input + '/json/') //abriu esse link com o input que o usuario colocar
+    ajax.send();
+
+    ajax.onload = function(){
+        // document.getElementById('texto').innerHTML = this.responseText; // mostra o cep do usuario no html
+        let obj = JSON.parse(this.responseText); //transforma texto em objeto
+
+        //Pegando elementos especificos
+        let logradouro = obj.logradouro;
+        let cidade = obj.localidade;
+        let estado = obj.uf; 
+
+        //mostrando os dados especificos
+        document.getElementById('texto').innerHTML = "Logradouro: " + logradouro + "<br> Cidade: " + cidade + "<br> Estado: " + estado;
+
+    }
+
+}
+
+
+ */
